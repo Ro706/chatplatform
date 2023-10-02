ce93c4f968e092a3a316290cb45f290996ce1bea
Ro706 <rohitmandal99091@gmail.com>
2023-10-02 23:19:05 +0530

    upadate:adding new file and modifying it

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio=require('socket.io');
+// const formatMessage = require('./utils/messages');
+const {
+    userJoin,
+    userLeave,
+    getCurrentUser,
+    getRoomUsers
+} = require('./utils/users');
+const formatMessage = require('./utils/messages');
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
@@ -9,11 +17,21 @@ const io=socketio(server);
 io.on('connection', (socket)=>{
     // socket represents an actual user
     // emit a endt to the client 
-    socket.emit("welcomemessage", "Welcome to the chat");
-    socket.on("connected", (data)=>{
-        console.log(data);
-        console.log(socket.id + " connected");
+    // socket.emit("welcomemessage", "Welcome to the chat");
+    // socket.on("connected", (data)=>{
+    //     console.log(data);
+    //     console.log(socket.id + " connected");
+    // })
+    socket.on("joinRoom",({username,room})=>{
+        // console.log(username,room);
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room)
+
+        // socket.emit("welcomemessage", "Welcome to the chat");
+        socket.emit('message',formatMessage("Admin","Welcome to the chat"));
     })
+        
+    
 });
 const port = 3000;
 
--- public/js/main.js
+++ public/js/main.js
@@ -17,3 +17,21 @@ const socket = io();
  
 // Join chatroom
 socket.emit('joinRoom', { username, room });
+
+socket.on('message',(message)=>{
+    outputMessage(message)
+})
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span> ${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
+}
