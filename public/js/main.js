c87ec0065d9932f3a39464d31f19008db86747ea
Ro706 <rohitmandal99091@gmail.com>
2023-10-02 22:37:21 +0530

    upadate:adding new file and modifying it

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,19 @@
-const socket = io();
-
-socket.on('welcomemessage', (data) => {
-    console.log(data);
+// const socket = io();
+// socket.on('welcomemessage', (data) => {
+//     console.log(data);
+// });
+// socket.emit("connected","The client connected");
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+ 
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
 });
-
-socket.emit("connected","The client connected");
+//  console.log(username,room);
+const socket = io();
+ 
+// Join chatroom
+socket.emit('joinRoom', { username, room });
