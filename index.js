a0836746993ddfc8a6e4181d24f6436dffb18c94
Ro706 <rohitmandal99091@gmail.com>
2023-10-03 05:55:55 +0530

    modified and fixing the bug

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on('connection', (socket)=>{
         socket.join(user.room);
      
         // Welcome current user
-        socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+        socket.emit("message", formatMessage(botName, "Welcome to command tech!"));
      
         // Broadcast when a user connects
         socket.broadcast
