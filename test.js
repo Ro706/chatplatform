8db5ec83871c5cd91c085b3b3bd1420da5352874
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2023-10-28 00:06:57 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+{
+  "name": "github-action-demo",
+  "version":"1.0.0",
+  "description":"",
+  "main":"index.js",
+  "scripts":{
+    "test":"jest"
+  },
+ "author":"",
+ "devDependencies":{
+  jest":"^27.4.3"
+ }
+}
